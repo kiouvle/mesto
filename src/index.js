ad20daa37ee0a0d912f0db7b790cc120793aab49
Jenya <zhenyamaltseva@ya.ru>
2019-12-22 18:28:03 +0300

    Clean up

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import CardList from './blocks/places-list/places-list.js';
 
   const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort5' : 'https://praktikum.tk/cohort5';
 
+  let cardsList = null;
+  const popupEnlarge = new PopupEnlarge(document.querySelector('.popup__enlarge'));
+
   const api = new Api({
     baseUrl: serverUrl,
     headers: {
@@ -23,9 +26,6 @@ import CardList from './blocks/places-list/places-list.js';
     })
     .catch((err) => console.log(err));
 
-  const popupEnlarge = new PopupEnlarge(document.querySelector('.popup__enlarge'));
-  let cardsList;
-
   //get cards from server
   api.getInitialCards()
     .then((result) => {
@@ -35,12 +35,11 @@ import CardList from './blocks/places-list/places-list.js';
     .catch((err) => console.log(err));
 
 
-
   //input check for button
   function inputCheck(first, second, buttonClass) {
     const popupButton = document.querySelector(buttonClass);
 
-    if ((first.length <= 1 || first.length > 30) || (second.length <= 1 || second.length > 30)) {
+    if ((first.length <= 1 || first.length > 300) || (second.length <= 1 || second.length > 30)) {
       popupButton.setAttribute('disabled', true);
       popupButton.removeAttribute('style');
       popupButton.setAttribute('style', 'margin-top: 33px');
@@ -56,11 +55,11 @@ import CardList from './blocks/places-list/places-list.js';
     const errJob = document.querySelector('.error-text__job');
     const inputJob = document.querySelector('.popup__input_type_job');
 
-    if (formData.elements.name.value.length === 0) {     //check name line
+    if (nameField.value.length === 0) {     //check name line
       errName.setAttribute('style', 'display: block');
       errName.textContent = 'Это обязательное поле';
       inputJob.setAttribute('style', 'margin-top: 9px');
-    } else if (formData.elements.name.value.length === 1 || formData.elements.name.value.length > 30) {
+    } else if (nameField.value.length === 1 || nameField.value.length > 30) {
       errName.setAttribute('style', 'display: block');
       errName.textContent = 'Должно быть от 2 до 30 символов';
       inputJob.setAttribute('style', 'margin-top: 7px');
@@ -69,10 +68,10 @@ import CardList from './blocks/places-list/places-list.js';
       inputJob.removeAttribute('style');
     }
 
-    if (formData.elements.job.value.length === 0) {  //check description line
+    if (jobField.length === 0) {  //check description line
       errJob.setAttribute('style', 'display: block');
       errJob.textContent = 'Это обязательное поле';
-    } else if (formData.elements.job.value.length === 1 || formData.elements.job.value.length > 30) {
+    } else if (jobField.value.length === 1 || jobField.value.length > 30) {
       errJob.setAttribute('style', 'display: block');
       errJob.textContent = 'Должно быть от 2 до 30 символов';
     } else {
@@ -81,16 +80,16 @@ import CardList from './blocks/places-list/places-list.js';
 
   };
 
+  const userName = document.querySelector('.user-info__name');
+  const userJob = document.querySelector('.user-info__job');
+  const userAvatar = document.querySelector('.user-info__photo');
+
   //editing user shit
   function editData(name, job, avatar) {
-    const userName = document.querySelector('.user-info__name');
     userName.textContent = name;
-
-    const userJob = document.querySelector('.user-info__job');
     userJob.textContent = job;
 
     if (avatar) {
-      const userAvatar = document.querySelector('.user-info__photo');
       userAvatar.setAttribute('style', 'background-image: url(' + avatar + ')');
     }
   }
@@ -98,41 +97,46 @@ import CardList from './blocks/places-list/places-list.js';
   //showing the form
   const popupCard = new Popup(document.querySelector('.popup__card'));
   const addButton = document.querySelector('.user-info__button');
-  addButton.addEventListener('click', function (event) {
-    popupCard.open();
-    inputCheck(form.elements.link.value, form.elements.name.value, '.popup__button_add');
-  });
-
-  const editButton = document.querySelector('.user-info__data-edit');
-  const popupData = new Popup(document.querySelector('.popup__data'));
-  editButton.addEventListener('click', function (event) {
-    popupData.open();
-    formData.elements.name.value = document.querySelector('.user-info__name').textContent;
-    formData.elements.job.value = document.querySelector('.user-info__job').textContent;
-    inputCheck(formData.elements.name.value, formData.elements.job.value, '.popup__button_data');
-  });
 
   //user cards addition
-  const form = document.forms.new;
+  const addCardForm = document.forms.new;
+  const placeNameField = addCardForm.elements.name;
+  const placePucUrlField = addCardForm.elements.link;
 
-  form.addEventListener('submit', function (event) {
+  addButton.addEventListener('click', () => {
+    popupCard.open();
+    inputCheck(placePucUrlField.value, placeNameField.value, '.popup__button_add');
+  });
+
+  addCardForm.addEventListener('submit', () => {
     event.preventDefault();
-    const form = event.currentTarget;
-    cardsList.addCard(form.elements.link.value, form.elements.name.value); //add user cards
-    popupCard.close();     //popup close after adding
+    cardsList.addCard(placePucUrlField.value, placeNameField.value); //add user cards
+    popupCard.close(); //popup close after adding
   });
 
   //button check for card form
-  form.addEventListener('keyup', function () { inputCheck(form.elements.link.value, form.elements.name.value, '.popup__button_add') }); //check user input
+  addCardForm.addEventListener('keyup', function () { inputCheck(placePucUrlField.value, placeNameField.value, '.popup__button_add') }); //check user input
 
-  //click the button and get ur shit
-  const formData = document.forms.data;
-  formData.addEventListener('submit', function (event) {
+  const editPersonalInfoForm = document.forms.data;
+  const jobField = editPersonalInfoForm.elements.job;
+  const nameField = editPersonalInfoForm.elements.name;
+
+  const editButton = document.querySelector('.user-info__data-edit');
+  const popupData = new Popup(document.querySelector('.popup__data'));
+
+  editButton.addEventListener('click', function (event) {
+    popupData.open();
+    nameField.value = document.querySelector('.user-info__name').textContent;
+    jobField.value = document.querySelector('.user-info__job').textContent;
+    inputCheck(nameField.value, jobField.value, '.popup__button_data');
+  });
+
+  //click the button and get ur shit  
+  editPersonalInfoForm.addEventListener('submit', () => {
     event.preventDefault();
-    const formData = event.currentTarget;
-    //post info to server
 
-    api.setUserData(formData.elements.name.value, formData.elements.job.value)
+    //post info to server
+    api.setUserData(nameField.value, jobField.value)
       .then((result) => {
         popupData.close();
         editData(result.name, result.about, result.avatar);
@@ -141,8 +145,8 @@ import CardList from './blocks/places-list/places-list.js';
   });
 
   //button check for user info
-  formData.addEventListener('keyup', function () { inputCheck(formData.elements.name.value, formData.elements.job.value, '.popup__button_data') });
+  editPersonalInfoForm.addEventListener('keyup', function () { inputCheck(nameField.value, jobField.value, '.popup__button_data') });
 
   //error text check for user info
-  formData.addEventListener('keyup', errorText);
+  editPersonalInfoForm.addEventListener('keyup', errorText);
 })();
